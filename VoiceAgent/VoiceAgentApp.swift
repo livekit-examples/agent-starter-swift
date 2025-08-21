d191e286061eaa2c970b254b6dbcd1316208eda7
Ben Cherry <ben@livekit.io>
2025-08-21 16:51:04 -0700

    add default window size on mac

--- VoiceAgent/VoiceAgentApp.swift
+++ VoiceAgent/VoiceAgentApp.swift
@@ -11,6 +11,9 @@ struct VoiceAgentApp: App {
             AppView()
                 .environment(viewModel)
         }
+        #if os(macOS)
+        .defaultSize(width: 900, height: 900)
+        #endif
         #if os(visionOS)
         .windowStyle(.plain)
         .windowResizability(.contentMinSize)
@@ -28,5 +31,5 @@ struct AgentFeatures: OptionSet {
     static let text = Self(rawValue: 1 << 1)
     static let video = Self(rawValue: 1 << 2)
 
-    static let current: Self = [.voice, .text]
+    static let current: Self = [.voice, .text, .video]
 }
